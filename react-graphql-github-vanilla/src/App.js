53ef802b9afd910e430426085bf72f6da894bfa4
Paul Tobin <tobster111@gmail.com>
2020-04-17 15:56:25 +0100

    Refactored to use hooks

--- react-graphql-github-vanilla/src/App.js
+++ react-graphql-github-vanilla/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
 import Organization from './components/Organization';
@@ -21,61 +21,55 @@ const GET_ORGANIZATION = `
 
 const TITLE = 'React GraphQL GitHub Client';
 
-class App extends Component {
-  state = {
-    path: 'the-road-to-learn-react/the-road-to-learn-react',
-    organization: null,
-    errors: null,
+const App = () => {
+  const [path, setPath] = useState(
+    'the-road-to-learn-react/the-road-to-learn-react'
+  );
+  const [organization, setOrganization] = useState(null);
+  const [errors, setErrors] = useState(null);
+
+  useEffect(() => {
+    onFetchFromGitHub();
+  }, []);
+
+  const onChange = (event) => {
+    setPath(event.target.value);
   };
 
-  componentDidMount() {
-    // fetch data
-    this.onFetchFromGitHub();
-  }
-
-  onChange = (event) => {
-    this.setState({ path: event.target.value });
-  };
-
-  onSubmit = (event) => {
+  const onSubmit = (event) => {
     // fetch data
     event.preventDefault();
   };
 
-  onFetchFromGitHub = () => {
-    axiosGitHubGraphQL.post('', { query: GET_ORGANIZATION }).then((result) =>
-      this.setState(() => ({
-        organization: result.data.data.organization,
-        error: result.data.error,
-      }))
-    );
+  const onFetchFromGitHub = () => {
+    axiosGitHubGraphQL.post('', { query: GET_ORGANIZATION }).then((result) => {
+      setOrganization(result.data.data.organization);
+      setErrors(result.data.errors);
+    });
   };
 
-  render() {
-    const { path, organization, errors } = this.state;
-    return (
-      <div>
-        <h1>{TITLE}</h1>
-        <form onSubmit={this.onSubmit}>
-          <label htmlFor="url">Show open issues for https://github.com/</label>
-          <input
-            id="url"
-            type="text"
-            value={path}
-            onChange={this.onChange}
-            style={{ width: '300px' }}
-          />
-          <button type="submit">Search</button>
-        </form>
-        <hr />
-        {organization ? (
-          <Organization organization={organization} errors={errors} />
-        ) : (
-          <p>No information yet ...</p>
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <h1>{TITLE}</h1>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="url">Show open issues for https://github.com/</label>
+        <input
+          id="url"
+          type="text"
+          value={path}
+          onChange={onChange}
+          style={{ width: '300px' }}
+        />
+        <button type="submit">Search</button>
+      </form>
+      <hr />
+      {organization ? (
+        <Organization organization={organization} errors={errors} />
+      ) : (
+        <p>No information yet ...</p>
+      )}
+    </div>
+  );
+};
 
 export default App;
--- react-graphql-github-vanilla/src/components/Organization.js
+++ react-graphql-github-vanilla/src/components/Organization.js
@@ -1,12 +1,24 @@
 import React from 'react';
 
-const Organization = ({ organization }) => (
-  <div>
-    <p>
-      <strong>Issues from Organization:</strong>
-      <a href={organization.url}>{organization.name}</a>
-    </p>
-  </div>
-);
+const Organization = ({ organization, errors }) => {
+  const { url, name } = organization;
+
+  if (errors) {
+    return (
+      <p>
+        <strong>Something went wrong:</strong>
+        {errors.map((error) => error.message).join(' ')}
+      </p>
+    );
+  }
+  return (
+    <div>
+      <p>
+        <strong>Issues from Organization:</strong>
+        <a href={url}>{name}</a>
+      </p>
+    </div>
+  );
+};
 
 export default Organization;
