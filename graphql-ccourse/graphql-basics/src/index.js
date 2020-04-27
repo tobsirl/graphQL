0a9f4ede9c46cfc715319e76cd5e32498960dd6d
Paul Tobin <tobster111@gmail.com>
2020-04-27 15:43:07 +0100

    Adding the posts array to the User type

--- graphql-ccourse/graphql-basics/src/index.js
+++ graphql-ccourse/graphql-basics/src/index.js
@@ -17,6 +17,7 @@ const typeDefs = `
     name: String!
     email: String!
     age: Int
+    posts: [Post!]!
   }
 
   type Post {
@@ -72,6 +73,13 @@ const resolvers = {
     author(parent, args, ctx, info) {
       return users.find((user) => {
         return user.id === parent.author;
+      });
+    },
+  },
+  User: {
+    posts(parent, args, ctx, info) {
+      return posts.filter((post) => {
+        return post.author === parent.id;
       });
     },
   },
