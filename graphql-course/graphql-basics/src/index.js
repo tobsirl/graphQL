5c89928773d45b3d16d482c497acaacb83e32347
Paul Tobin <tobster111@gmail.com>
2020-04-30 17:34:00 +0100

    Added a createPost mutation

--- graphql-course/graphql-basics/src/index.js
+++ graphql-course/graphql-basics/src/index.js
@@ -16,6 +16,7 @@ const typeDefs = `
 
   type Mutation {
     createUser(name: String!, email: String!, age: Int): User!
+    createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
   }
 
   type User {
@@ -106,9 +107,27 @@ const resolvers = {
       };
 
       // save the user
-      users.concat(user);
+      users.push(user);
 
       return user;
+    },
+    createPost(parent, args, ctx, info) {
+      // check if the user exists
+      const userExists = users.some((user) => user.id === args.author);
+
+      if (!userExists) throw new Error(`User not found`);
+
+      const post = {
+        id: uuidv4(),
+        title: args.title,
+        body: args.body,
+        published: args.published,
+        author: args.author,
+      };
+
+      posts.push(post);
+
+      return post;
     },
   },
   Post: {
