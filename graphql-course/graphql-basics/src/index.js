171923f1a16f8b29e24feefc75980d252a1dc014
Paul Tobin <tobster111@gmail.com>
2020-04-30 18:28:43 +0100

    Added a createComment mutation

--- graphql-course/graphql-basics/src/index.js
+++ graphql-course/graphql-basics/src/index.js
@@ -17,6 +17,7 @@ const typeDefs = `
   type Mutation {
     createUser(name: String!, email: String!, age: Int): User!
     createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
+    createComment(text: String!, author: ID!, post: ID!): Comment!
   }
 
   type User {
@@ -128,6 +129,26 @@ const resolvers = {
       posts.push(post);
 
       return post;
+    },
+    createComment(parent, args, ctx, info) {
+      // check if the user exists
+      const userExists = users.some((user) => user.id === args.author);
+      const postExists = posts.some(
+        (post) => post.id === args.post && post.published === true
+      );
+      
+      if (!userExists || !postExists) throw new Error(`User not found`);
+
+      const comment = {
+        id: uuidv4(),
+        text: args.text,
+        author: args.author,
+        post: args.post,
+      };
+
+      comments.push(comment);
+
+      return comment;
     },
   },
   Post: {
