01c757577a7afa49e6043cbe51cb1a0c4e957ce7
Paul Tobin <tobster111@gmail.com>
2020-05-13 16:27:32 +0100

    Example of using prisma binding with promises and async/await

--- graphql-course/graphql-prisma/src/prisma.js
+++ graphql-course/graphql-prisma/src/prisma.js
@@ -5,10 +5,20 @@ const prisma = new Prisma({
   endpoint: 'http://192.168.99.100:4466/',
 });
 
-prisma.query.users(null, '{ id name email posts { id title } }').then((data) => {
-  console.log(JSON.stringify(data, null, 2));
-});
+// Prisma Binding Queries
+// prisma.query.users(null, '{ id name email posts { id title } }').then((data) => {
+//   console.log(JSON.stringify(data, null, 2));
+// });
 
-prisma.query.comments(null, `{ id text author { id name } }`).then((data) => {
-  console.log(JSON.stringify(data, null, 2));
-})
+// prisma.query.comments(null, `{ id text author { id name } }`).then((data) => {
+//   console.log(JSON.stringify(data, null, 2));
+// });
+
+// async function getPosts() {
+//   const data = await prisma.query.posts(null, `{ id title body }`)
+
+//   console.log(JSON.stringify(data, null, 2));
+// }
+
+// getPosts()
+
