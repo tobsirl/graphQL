300176492745cb8b7524bb9271b4fb659ed373de
Paul Tobin <tobster111@gmail.com>
2020-05-13 15:47:21 +0100

    Example using prisma bindings

--- graphql-course/graphql-prisma/src/index.js
+++ graphql-course/graphql-prisma/src/index.js
@@ -1,4 +1,5 @@
 import { GraphQLServer, PubSub } from 'graphql-yoga';
+import './prisma';
 
 import db from './db';
 import Query from './resolvers/Query';
--- graphql-course/graphql-prisma/src/prisma.js
+++ graphql-course/graphql-prisma/src/prisma.js
@@ -1,6 +1,10 @@
 import { Prisma } from 'prisma-binding';
 
 const prisma = new Prisma({
-  typeDefs: '/src/generated/prisma.graphql',
-  endpoint: 'http://192.168.99.100:4466/'
-})
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: 'http://192.168.99.100:4466/',
+});
+
+prisma.query.users(null, '{ id name email posts { id title } }').then((data) => {
+  console.log(JSON.stringify(data, null, 2));
+});
