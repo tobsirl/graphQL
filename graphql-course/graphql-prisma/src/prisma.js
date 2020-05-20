75f2e0fc468f3bbb93a6173d3e7805649efce10d
Paul Tobin <tobster111@gmail.com>
2020-05-20 16:31:45 +0100

    Added a secret to lockdown the backend, secret added to prisma.yml and prisma.js

--- graphql-course/graphql-prisma/src/prisma.js
+++ graphql-course/graphql-prisma/src/prisma.js
@@ -3,6 +3,7 @@ import { Prisma } from 'prisma-binding';
 const prisma = new Prisma({
   typeDefs: 'src/generated/prisma.graphql',
   endpoint: 'http://192.168.99.100:4466/',
+  secret: `thecakeisalie`,
 });
 
 export default prisma;
