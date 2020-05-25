e73a270b1c9c6242452afd4812c7a9481e9a8090
Paul Tobin <tobster111@gmail.com>
2020-05-25 18:06:33 +0100

    Using User.js to hide fields from users who are not logged in

--- graphql-course/graphql-prisma/src/resolvers/User.js
+++ graphql-course/graphql-prisma/src/resolvers/User.js
@@ -1,3 +1,15 @@
-const User = {};
+import getUserId from '../utils/getUserId';
+
+const User = {
+  email(parent, args, { request }, info) {
+    const userId = getUserId(request, false);
+
+    if (userId && parent.id === userId) {
+      return parent.email;
+    } else {
+      return null;
+    }
+  },
+};
 
 export default User;
