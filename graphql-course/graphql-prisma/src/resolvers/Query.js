653e915edab178651018417f13665e9f6eb0ba2f
Paul Tobin <tobster111@gmail.com>
2020-05-27 15:38:13 +0100

    Pagination added after to both posts and users

--- graphql-course/graphql-prisma/src/resolvers/Query.js
+++ graphql-course/graphql-prisma/src/resolvers/Query.js
@@ -5,6 +5,7 @@ const Query = {
     const opArgs = {
       first: args.first,
       skip: args.skip,
+      after: args.after,
     };
 
     if (args.query) {
@@ -22,6 +23,7 @@ const Query = {
     const opArgs = {
       first: args.first,
       skip: args.skip,
+      after: args.after,
       where: {
         author: {
           id: userId,
