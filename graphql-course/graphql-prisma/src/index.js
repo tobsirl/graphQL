b2d5fd910e6ffd618fa66c96531faa2f9bd48b52
Paul Tobin <tobster111@gmail.com>
2020-05-29 17:32:32 +0100

    Installed babel/polyfill and env-cmd

--- graphql-course/graphql-prisma/src/index.js
+++ graphql-course/graphql-prisma/src/index.js
@@ -1,3 +1,4 @@
+import '@babel/polyfill';
 import { GraphQLServer, PubSub } from 'graphql-yoga';
 import prisma from './prisma';
 import { resolvers, fragmentReplacements } from './resolvers/index';
