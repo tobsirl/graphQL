0c750531c42590490adac05fed1737083da6ebff
Paul Tobin <tobster111@gmail.com>
2020-05-29 19:51:49 +0100

    Imported ApolloProvider from @apollo/react-hooks and setup ApolloClient

--- pokimon-graphql-react/src/App.js
+++ pokimon-graphql-react/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
 
 import './App.css';
 
 function App() {
+  const client = new ApolloClient({
+    uri: `https://graphql-pokemon.now.sh`,
+  });
+
   return (
-    <div className="App">
-      <h1>Pokimon</h1>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h1>Pokimon</h1>
+      </div>
+    </ApolloProvider>
   );
 }
 
