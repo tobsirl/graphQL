5ecad49fe9d5629c4d55388e27979598feaded6c
Paul Tobin <tobster111@gmail.com>
2020-06-01 19:55:47 +0100

    Pokemon component fleshed out

--- pokimon-graphql-react/src/components/Pokemon.js
+++ pokimon-graphql-react/src/components/Pokemon.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
-export default function Pokemon(props) {
+export default function Pokemon({ pokemon }) {
   return (
-    <div>
-      <p>{props.pokemon.name}</p>
+    <div className="pokemon">
+      <div className="pokemon__name">
+        <p>{pokemon.name}</p>
+      </div>
+      <div className="pokemon__meta">
+        <span>{pokemon.maxHP}</span>
+        <span>{pokemon.maxCP}</span>
+      </div>
+      <div className="pokemon__image">
+        <img
+          className="pokemon__image"
+          src={pokemon.image}
+          alt={pokemon.name}
+        />
+      </div>
+      <div class="pokemon__attacks"></div>
     </div>
   );
 }
