541737aafa4a0268734cb7317f38fd996f50d7b8
Paul Tobin <tobster111@gmail.com>
2020-06-01 19:33:42 +0100

    Mapping over the pokemons array and using key and prop to Pokemon component

---
+++ pokimon-graphql-react/src/components/Pokemon.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Pokemon() {
+  return (
+    <div>
+      <p>Pokemon</p>
+    </div>
+  );
+}
--- pokimon-graphql-react/src/containers/PokemonsContainer.js
+++ pokimon-graphql-react/src/containers/PokemonsContainer.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { pokemon } from '../components/Pokemon';
 
 export default function PokemonsContainer() {
+  const { data: { pokemons = [] } = {} } = useQuery(GET_POKEMONS, {
+    variables: { first: 9 },
+  });
   return (
-    <div>
-      <p>I am a pokemon!</p>
+    <div className="container">
+      {pokemons &&
+        pokemons.map((pokemon) => (
+          <Pokemon key={pokemon.id} pokemon={pokemon} />
+        ))}
     </div>
   );
 }
