58a1c67e862ac4e5418b7abc4834b35869cfd70e
Paul Tobin <tobster111@gmail.com>
2020-06-04 14:34:07 +0100

    Setup globalSetup.js and globalTeardown for setting up jest and tearing it down after running

--- graphql-course/graphql-prisma/src/index.js
+++ graphql-course/graphql-prisma/src/index.js
@@ -1,25 +1,5 @@
 import '@babel/polyfill/noConflict';
-import { GraphQLServer, PubSub } from 'graphql-yoga';
-import prisma from './prisma';
-import { resolvers, fragmentReplacements } from './resolvers/index';
-
-import db from './db';
-
-const pubsub = new PubSub();
-
-const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
-  resolvers,
-  context(request) {
-    return {
-      db,
-      pubsub,
-      prisma,
-      request,
-    };
-  },
-  fragmentReplacements,
-});
+import server from './server';
 
 server.start(
   {
